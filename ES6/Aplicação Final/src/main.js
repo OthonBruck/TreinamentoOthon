fbb65ef8a312b2c91b98919ab7bd0e60ae9b4873
OthonBruck <othon.bruck@hotmail.com>
2020-08-31 15:42:38 -0300

    Adicionando repositorios

--- ES6/Aplicação Final/src/main.js
+++ ES6/Aplicação Final/src/main.js
@@ -1,64 +1,28 @@
-// 1.1
+class App {
+  constructor() {
+    this.repositories = [];
 
-const delay = () => new Promise((resolve) => setTimeout(resolve, 1000));
+    this.formEl = document.getElementById("repo-form");
 
-async function umPorSegundo() {
-  await delay();
-  console.log("1s");
+    this.registerHandlers();
+  }
 
-  await delay();
-  console.log("2s");
+  registerHandlers() {
+    this.formEl.onsubmit = (event) => this.addRepository(event);
+  }
 
-  await delay();
-  console.log("3s");
-}
+  addRepository(event) {
+    event.preventDefault();
 
-umPorSegundo();
+    this.repositories.push({
+      name: "rocketseat.com.br",
+      description: "Tire a sua ideia do papel e dê vida a sua startup.",
+      avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
+      html_url: "https://github.com/Rocketseat/gatsby-starter-rocket-docs",
+    });
 
-// 1.2
-
-import axios from "axios";
-
-async function getUserFromGithub(user) {
-  try {
-    const response = await axios.get(`https://api.github.com/users/${user}`);
-
-    console.log(response.data);
-  } catch (err) {
-    console.log("Usuário não existe");
+    console.log(this.repositories);
   }
 }
 
-getUserFromGithub("diego3g");
-getUserFromGithub("diego3g124123");
-
-// 1.3
-
-class Github {
-  static async getRepositories(repo) {
-    try {
-      const response = await axios.get(`https://api.github.com/repos/${repo}`);
-
-      console.log(response.data);
-    } catch (err) {
-      console.log("Repositório não existe");
-    }
-  }
-}
-
-Github.getRepositories("rocketseat/rocketseat.com.br");
-Github.getRepositories("rocketseat/dslkvmskv");
-
-// 1.4
-
-const buscaUsuario = async (usuario) => {
-  try {
-    const response = await axios.get(`https://api.github.com/users/${usuario}`);
-
-    console.log(response.data);
-  } catch (err) {
-    console.log("Usuário não existe");
-  }
-};
-
-buscaUsuario("diego3g");
+new App();
