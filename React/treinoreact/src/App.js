7b53fe49d93c53930c538f0c527bb5afca211915
OthonBruck <othon.bruck@hotmail.com>
2020-09-01 13:54:22 -0300

    feat: Inserindo a navegação para detalhes do produto

--- React/treinoreact/src/App.js
+++ React/treinoreact/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Main from "./pages/main";
+import Routes from "./routes";
 
 import "./styles.css";
 
 import Header from "./components/Header";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 }
