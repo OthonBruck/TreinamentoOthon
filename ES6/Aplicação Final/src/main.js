62d2393856db30bad4467699c949af1653f46938
OthonBruck <othon.bruck@hotmail.com>
2020-09-01 09:36:30 -0300

    feat: Tratamento de erros e loadings

--- ES6/Aplicação Final/src/main.js
+++ ES6/Aplicação Final/src/main.js
@@ -17,6 +17,18 @@ class App {
     this.formEl.onsubmit = (event) => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement("span");
+      loadingEl.appendChild(document.createTextNode("Carregando"));
+      loadingEl.setAttribute("id", "loading");
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById("loading").remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
@@ -24,25 +36,32 @@ class App {
 
     if (repoInput.length === 0) return;
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    const {
-      name,
-      description,
-      html_url,
-      owner: { avatar_url },
-    } = response.data;
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url },
+      } = response.data;
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-    });
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
 
-    this.inputEl.value = "";
+      this.inputEl.value = "";
 
-    this.render();
+      this.render();
+    } catch (err) {
+      alert("O repositorio nao existe");
+    }
+
+    this.setLoading(false);
   }
 
   render() {
